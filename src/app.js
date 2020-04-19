7ad9579e10c34086942e82afb7c9b47e37e5c289
Javier Ledezma <jledezma@medtrainer.com>
2020-04-18 20:27:47 -0500

    Installing socket io and pug as view engine

--- src/app.js
+++ src/app.js
@@ -1,10 +1,24 @@
-var express = require('express')
-var app = express()
+const express = require('express')
+const http = require('http')
+const socketIo = require('socket.io')
+const path = require('path')
+
+const app = express()
+const server = http.Server(app)
+const io = socketIo(server)
+
+app.use(express.static('public'));
+app.set('views', path.join(__dirname, '/views'));
+app.set('view engine', 'pug')
 
 app.get('/', function(req, res) {
-  res.send('Hello world')
+  res.render('index')
 })
 
-app.listen(3000, function() {
+io.on('connect', function() {
+  console.log('Hello world from socket.io')
+})
+
+server.listen(3000, function() {
   console.log('Example app listening on port 3000')
 })
