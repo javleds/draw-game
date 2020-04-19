1b3fb777e77a329ef4493c32c8dc91c9658e6ad8
Javier Ledezma <jledezma@medtrainer.com>
2020-04-18 21:28:09 -0500

    Add render function

--- public/js/game.js
+++ public/js/game.js
@@ -3,14 +3,22 @@
     serverUrl: 'http://localhost:3000',
     players: 1,
     code: '',
+    idPlayer: '',
+
+    render: function() {
+      const $players = document.getElementById('players')
+      const $code = document.getElementById('code')
+      $players.innerHTML = this.players
+      $code.innerHTML = this.code
+    },
 
     init: function() {
-      this.code = document.getElementById('code').value;
-      const socket = io.connect(this.serverUrl);
-
-      socket.emit('connect', {
-        code: this.code
-      })
+      this.code = document.getElementById('codeInput').value
+      // const socket = io.connect(vm.serverUrl);
+      // socket.emit('connect', {
+      //   code: vm.code
+      // })
+      this.render()
     }
   }
 
